abb0a0562c29275828052782a318f5ff5579797e
griffinbaker12 <griffin_baker12@icloud.com>
2022-05-05 15:13:39 -0500

    final code

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ import createSagaMiddleware from '@redux-saga/core';
 import Counter from './Counter';
 import reducer from './reducers';
 
-import { helloSaga } from './sagas';
+import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(helloSaga);
+sagaMiddleware.run(rootSaga);
 
 const action = type => store.dispatch({ type });
 
--- sagas.js
+++ sagas.js
@@ -1,5 +1,19 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery, all } from 'redux-saga/effects';
 
-export function* helloSaga() {
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function* helloSaga() {
   console.log('Hello Sagas!');
+}
+function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: 'INCREMENT' });
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+}
+
+export default function* rootSaga() {
+  yield all([helloSaga(), watchIncrementAsync()]);
 }
